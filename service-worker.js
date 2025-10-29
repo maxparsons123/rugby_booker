8d45c170e0d771c543ba39db96a57a6c445e2949
maxparsons123 <115324986+maxparsons123@users.noreply.github.com>
2025-10-29 20:11:58 +0000

    Update service worker for new cache version and assets

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,34 @@
-const CACHE_NAME='bcu-albany-v1';
-const ASSETS=[
-  './index.html','./manifest.json',
-  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
-  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
-  'https://unpkg.com/mqtt/dist/mqtt.min.js'
+const CACHE_NAME = "bcu-rugby-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css",
+  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js",
+  "https://unpkg.com/mqtt/dist/mqtt.min.js"
 ];
 
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
-  self.skipWaiting();
+// Install
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
 });
-self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));
-  self.clients.claim();
-});
-self.addEventListener('fetch',e=>{
+
+// Fetch
+self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html')))
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
+
+// Activate
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
   );
 });
